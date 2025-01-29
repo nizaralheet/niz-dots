e761a8e04f87756a234030dd8fc8c884bf6e4055
niz <156370431+nizaralheet@users.noreply.github.com>
2025-01-29 20:29:49 +0300

    Update autostart.sh
    removed unnecessary things

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 picom &
-#wal -R ;
+wal -R ;
 nohup skippy-xd --start-daemon &
 nohup dunst &
 firewall-applet &
 diodon &
 plank &
 ./.config/qtile/idle-manager.sh &
-#xinput set-prop 9 317 0 &&
